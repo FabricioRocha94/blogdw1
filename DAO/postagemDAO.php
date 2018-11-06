731a766b1e3f332d5531b55fa2cfc911777fa165
Fabricio Rocha <fabricio10.90@hotmail.com>
2018-11-06 19:33:50 -0200

    Iniciado a listagem de posts.

--- DAO/postagemDAO.php
+++ DAO/postagemDAO.php
@@ -13,25 +13,6 @@ function listarPosts()
     $conexao = conectar();
     $select = executar($conexao, "SELECT * FROM POSTAGEM");
     $desconectar = desconectar($conexao);
-
-    $linha = retorna_linha($select);
-    $total = verifica_resultado($select);
-
-    // se o número de resultados for maior que zero, mostra os dados
-    if ($total > 0) {
-        // inicia o loop que vai mostrar todos os dados
-        do {
-            ?>
-            <p><?= $linha['ID'] ?> / <?= $linha['TEXTO'] ?></p>
-<?php
-        // finaliza o loop que vai mostrar os dados
-} while ($linha = retorna_linha($select));
-    // fim do if 
-}
-?>
-</body>
-</html>
-<?php
-
+    return $select;
 }
 ?>
--- postagem.php
+++ postagem.php
@@ -3,6 +3,22 @@
 require_once 'Model/post.php';
 require_once 'DAO/postagemDAO.php';
 
-listarPosts();
+function posts()
+{
+    $select = listarPosts();
+    $linha = retorna_linha($select);
+    $total = verifica_resultado($select);
 
+    // se o número de resultados for maior que zero, mostra os dados
+    if ($total > 0) {
+        // inicia o loop que vai mostrar todos os dados
+        do {
+            ?>
+            <p><?= $linha['ID'] ?> / <?= $linha['TEXTO'] ?></p>
+<?php
+        // finaliza o loop que vai mostrar os dados
+} while ($linha = retorna_linha($select));
+    // fim do if 
+}
+}
 ?>
